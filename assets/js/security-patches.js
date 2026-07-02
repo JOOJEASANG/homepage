ff0627259da408b624ab80879a2c77b7827e09fc
주재상 <joojeasang@gmail.com>
2026-07-02 16:26:04 +0900

    feat: load book item unit price patch

--- assets/js/security-patches.js
+++ assets/js/security-patches.js
@@ -16,6 +16,7 @@ try {
   if (CURRENT_FILE === 'quote-book.html') {
     import('./wire-cover-patch.js').catch(() => null);
     import('./book-no-binding-cover-patch.js').catch(() => null);
+    import('./book-item-unit-price-patch.js').catch(() => null);
   }
 } catch (_) {}
 
